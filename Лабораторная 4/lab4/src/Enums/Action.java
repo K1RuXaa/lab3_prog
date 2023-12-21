1e7cefba2b88d97a51b710e9aa2f23e952486ec0
K1RuXaa <148982222+K1RuXaa@users.noreply.github.com>
2023-12-21 23:07:26 +0300

    Add files via upload

---
+++ Лабораторная 4/lab4/src/Enums/Action.java
@@ -0,0 +1,5 @@
+package Enums;
+
+public enum Action {
+    SNORT, SPLASH, NOTENOUGH, SOUND, THINK, RUN, OPEN, WANT, MEET, THOUGHT, CAN, SLEEP, STAY, CONFUSE, Watch, noDelete,Put,Clogged,LeanedOut,DropOut,Throw,Swim,Hurt,Seem,Stand,Understood,See,Done,Could
+}
